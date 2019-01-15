c27e7cdd3cc7a60d12cb8c52f6137f340fe90504
Andreea Stroe <aandreeastroe@yahoo.com>
2019-01-16 00:31:28 +0200

    initial commit

---
+++ cOdin/utilities/preprocessor.py
@@ -0,0 +1,7 @@
+#  implement the input and output data files processing (0.5 point)
+#  provide data transformation functionality, such as matrix centering, standardization, filling-in the NA, NaN cells etc. (0.5 points)
+import pandas as pd
+
+def readDataframeFromFile(filePath):
+    df
+    return df
