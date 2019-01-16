e408c6cf8a96ef91625f9f22f9dd12a259588ad2
Andreea Stroe <aandreeastroe@yahoo.com>
2019-01-16 13:53:43 +0200

    r

--- cOdin/utilities/preprocessor.py
+++ cOdin/utilities/preprocessor.py
@@ -2,10 +2,37 @@
 #  provide data transformation functionality, such as matrix centering, standardization, filling-in the NA, NaN cells etc. (0.5 points)
 import pandas as pd
 import numpy as np
+import scipy as sp
 
+# Replaces NA values in a numpy.ndarray with the average value on the column they belong to
+def replaceNAValues(values):
+    averages = np.nanmean(values, axis=0)   # Computes the arithmetic mean along the specified axis, ignoring NaNs
+    positions = np.where(np.isnan(values)) # array of positions with NA values
+    values[positions] = averages[positions[1]]
+    return values
+
+# Replaces NA values in a numpy.ndarray with the mean/mode value on the column they belong to
+def replaceNANValuesDataFrame(values):
+    for c in values.columns:
+        if pd.api.types.is_numeric_dtype(values[c]):
+            averages = np.nanmean(values, axis=0)  # Computes the arithmetic mean along the specified axis, ignoring NaNs
+            positions = np.where(np.isnan(values))  # array of positions with NA values
+            values[positions] = averages[positions[1]]
+            return values
+        else:
+            #nu garantez ca merge
+            if values[c].isna().any():
+                values = values[~np.isnan(values)]
+                mode = sp.stats.mode(values[c])
+                # mode = values[c].mode()
+                values[c] = values[c].fillna(mode[0])
+
+# Reads data from a .csv file to a pandas.DataFrame
 def readDataframeFromCSV(filePath, replaceNA = False):
     try:
         data = pd.read_csv(filePath, index_col=1);
+        print(data)
+        print(data[data.columns[1:]])
         obs_name = data.index   # stores the observations' names from the data
         var_name = data.columns[1:]     # stores the variables' names from the data
         values = data[var_name].values  # get the actual values from the data for the variable names
@@ -19,12 +46,28 @@ def readDataframeFromCSV(filePath, replaceNA = False):
         print("exit")
     return data
 
-# Replaces NA values in a numpy.ndarray with the average value on the column they belong to
-def replaceNAValues(values):
-    averages = np.nanmean(values, axis=0)   # Computes the arithmetic mean along the specified axis, ignoring NaNs
-    positions = np.where(np.isnan(values)) # array of positions with NA values
-    values[positions] = averages[positions[1]]
-    return values
+# Reads data from a .txt file to a pandas.DataFrame
+def readDataframeFromTXT(filePath, replaceNA = False):
+    try:
+        data = pd.read_fwf(filePath, index_col=1);
+        obs_name = data.index   # stores the observations' names from the data
+        var_name = data.columns[1:]     # stores the variables' names from the data
+        values = data[var_name].values  # get the actual values from the data for the variable names
+        if replaceNA == True:   # if the replaceNA parameter is specified with the value True, it makes a call to the replaceNAValues function
+            replaceNAValues(values)
+            data[var_name].values = values  #sets back the cleaned values in the data variable
+
+    except pd.errors.EmptyDataError: #Exception that is thrown in pd.read_csv (by both the C and Python engines) when empty data or header is encountered.
+        print("File not found or path is incorrect. Make sure it is a .csv file and exists at the specified destination. ")
+    finally:
+        print("exit")
+    return data
+
+# Writes a pandas.DataFrame to a .csv file
+def writeDataFrameToCSV(data, filePath = "data.csv"):
+    data.to_csv(path_or_buf = filePath)
+    print("Data was saved to " + filePath)
+
 
 # Standardize the column (variable) values for a pandas.DataFrame - luat de la Vinte
 def standize(values):
@@ -32,3 +75,50 @@ def standize(values):
     stdandardDevs = np.std(values, axis = 0)    # computes standard deviations for the values in the columns
     Xstd = (values - averages) / stdandardDevs  # standardizes each value in the initial data input
     return Xstd
+
+
+# http://rasbt.github.io/mlxtend/user_guide/preprocessing/standardize/
+def standardize(array):
+    ary_new = array.astype(float)
+    dim = ary_new.shape
+    if len(dim) == 1:
+        ary_new = ary_new[:, np.newaxis]
+
+    if isinstance(ary_new, pd.DataFrame):
+        ary_newt = ary_new.loc
+        columns = ary_new.columns
+    elif isinstance(ary_new, np.ndarray):
+        ary_newt = ary_new
+        columns = list(range(ary_new.shape[1]))
+    else:
+        raise AttributeError('Input array must be a pandas.DataFrame or numpy.ndarray')
+
+    parameters = {'avgs': ary_newt[:, columns].mean(axis=0),
+                  'stds': ary_newt[:, columns].std(axis=0, ddof=ddof)}
+    are_constant = np.all(ary_newt[:, columns] == ary_newt[0, columns], axis=0)
+    for c, b in zip(columns, are_constant):
+        if b:
+            ary_newt[:, c] = np.zeros(dim[0])
+            parameters['stds'][c] = 1.0
+
+    ary_newt[:, columns] = ((ary_newt[:, columns] - parameters['avgs']) /
+                            parameters['stds'])
+    return ary_newt[:, columns] # returns pandas.DataFrame, copy of the array or DataFrame with standardized columns
+
+
+def inverse(t, y=None):
+    if type(t) is pd.DataFrame:
+        for c in t.columns:
+            minim = t[c].min();
+            maxim = t[c].max()
+            if abs(minim) > abs(maxim):
+                t[c] = -t[c]
+                if y is not None:
+                    k = t.columns.get_loc(c)
+                    y[:, k] = -y[:, k]
+    else:
+        for i in range(np.shape(t)[1]):
+            minim = np.min(t[:, i]);
+            maxim = np.max(t[:, i])
+            if np.abs(minim) > np.abs(maxim):
+                t[:, i] = -t[:, i]
